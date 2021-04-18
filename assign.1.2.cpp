cb3bd3921291f6e4d6df216e717eca0804ef8fa8
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-04-18 17:42:23 +0530

    To find the area of circle.

---
+++ assign.1.2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float radius = 5 ,area;
+		
+	area = 3.14 * radius * radius;
+	
+	cout <<"\n area of circle : "<<area;
+
+	return (0);
+}
+
